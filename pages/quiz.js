2137d7b88d93664efb17b03b363cef10b70253b5
Maurício Rinardo <mauricio.rinardo@gmail.com>
2021-01-27 22:35:29 -0300

    Corrige bug na obtenção do valor da query string.

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import QuizBackground from '../src/components/QuizBackground';
 import GitHubCorner from '../src/components/GitHubCorner';
 import db from '../db.json';
 
 export default function Quiz() {
-  const params = new URLSearchParams(window.location.search);
-  const name = params.get('name');
+  const router = useRouter();
+  const { name } = router.query;
 
   return (
     <QuizBackground backgroundImage={db.bg}>
